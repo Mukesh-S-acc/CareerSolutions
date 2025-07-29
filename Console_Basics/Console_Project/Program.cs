bca399285ef22b4a028a66a2cc583b8282233a89
SaiDurga <saiprasadveluri@gmail.com>
2025-07-29 17:56:12 +0000

    First question completion

--- Console_Basics/Console_Project/Program.cs
+++ Console_Basics/Console_Project/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //1
+        Console.WriteLine("Hello, C#");
+
+
+        //2
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Welcome, " + name + "!");
+
+        //3
+        int age = 25;                
+        string userName = "EY";   
+        double height = 5.9;         
+
+        Console.WriteLine("\nUser Info:");
+        Console.WriteLine("Name   : " + userName);
+        Console.WriteLine("Age    : " + age + " years");
+        Console.WriteLine("Height : " + height + " feet");
+
+     
+    }
+}
